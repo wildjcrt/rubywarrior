39a1de5800059f67452d24c2398ef09a0d4cd295
Jerry Lee <jcrt@pixnet.tw>
2010-11-07 04:54:57 +0800

    level 4解法

--- jcrt-beginner/player.rb
+++ jcrt-beginner/player.rb
@@ -1,11 +1,29 @@
 class Player
+  def initialize
+    @health = 20
+  end
+  
   def play_turn(warrior)
-    if warrior.feel.enemy?
-      warrior.attack!
-    elsif warrior.health < 20
-      warrior.rest!
+    if archer_detect(warrior.health)
+      if warrior.feel.enemy?
+        warrior.attack!
+      elsif warrior.health < 20
+        warrior.rest!
+      else
+        warrior.walk!
+      end
     else
-      warrior.walk!
-    end  
+      if warrior.feel.enemy?
+        warrior.attack!
+      else
+        warrior.walk!
+      end
+    end
+    
+    @health = warrior.health
+  end
+  
+  def archer_detect(current_health)
+    (@health > current_health) ? (return true) : (return false)
   end
 end
