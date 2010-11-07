691a8d489eac2472b5e926cc359b2e2ba58a702b
Jerry Lee <jcrt@pixnet.tw>
2010-11-08 00:48:01 +0800

    level 6 救captive解法

--- jcrt-beginner/player.rb
+++ jcrt-beginner/player.rb
@@ -1,31 +1,47 @@
 class Player
   def initialize
     @health = 20
+    @rescue_captive = false
   end
   
   def play_turn(warrior)
-    if archer_detect(warrior.health)
+    unless @rescue_captive
+      if warrior.feel(:backward).captive?
+        warrior.rescue!(:backward)
+        @rescue_captive = true
+      else
+        warrior.walk!(:backward)
+      end
+      
+      return true
+    end
+    
+    if archer_detect(warrior)
       if warrior.feel.enemy?
         warrior.attack!
+      elsif warrior.health < 10
+        warrior.walk!(:backward)
       else
         warrior.walk!
       end
     else
       if warrior.feel.enemy?
         warrior.attack!
-      elsif warrior.feel.captive?
-        warrior.rescue!
       elsif warrior.health < 20
         warrior.rest!
       else
         warrior.walk!
       end
     end
-    
-    @health = warrior.health
   end
   
-  def archer_detect(current_health)
-    (@health > current_health) ? (return true) : (return false)
+  def archer_detect(warrior)
+    if @health > warrior.health
+      @health = warrior.health
+      return true
+    else
+      @health = warrior.health
+      return false
+    end
   end
 end
